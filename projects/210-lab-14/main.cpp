5a0a8544be03c3c9c3a00748cb6b18a92aa3cb68
1080heidi <heidideleon415@gmail.com>
2025-09-27 16:59:46 -0700

    First Update

--- projects/210-lab-14/main.cpp
+++ projects/210-lab-14/main.cpp
@@ -1,1 +1,19 @@
-#
+#include <iostream>
+using namespace std;
+
+// starting w struct, will change to class
+struct Color {
+    int r, g, b;
+    void print() {
+        cout << "Color(" << r << ", " << g << ", " << b << ")\n";
+    }
+};
+
+int main() {
+    Color c1{255, 0, 0}; // testing colors
+    Color c2{0, 255, 0};
+    c1.print();
+    c2.print();
+    return 0;
+}
+
