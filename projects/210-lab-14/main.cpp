0dc035e50df43f291999f0fc6a6fcc1ff9c18607
1080heidi <heidideleon415@gmail.com>
2025-09-27 17:08:58 -0700

    Second Update

--- projects/210-lab-14/main.cpp
+++ projects/210-lab-14/main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 // starting w struct, will change to class
 struct Color {
     int r, g, b;
+    
+    void setRed(int value) { r = value }
+    void setGreen(int value) { g = value; }
+    void setBlue(int value) { b = value; }
+    
+    int getRed() { return r;}
+    int getGreen() { return g; }
+    int getBlue() { return b; }
+     
     void print() {
         cout << "Color(" << r << ", " << g << ", " << b << ")\n";
     }
@@ -11,7 +20,11 @@ struct Color {
 
 int main() {
     Color c1{255, 0, 0}; // testing colors
-    Color c2{0, 255, 0};
+    Color c2;
+    c2.setRed(0);
+    c2.setGreen(255);
+    c2.setBlue(0);
+    
     c1.print();
     c2.print();
     return 0;
