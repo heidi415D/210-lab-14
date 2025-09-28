eb05f03c00b37c8de2a0b3d45880df769a198547
1080heidi <heidideleon415@gmail.com>
2025-09-27 17:29:08 -0700

    Changed to class - Update

--- projects/210-lab-14/main.cpp
+++ projects/210-lab-14/main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 using namespace std;
 
+const int ZERO_VAL = 0;
+const int RED_VAL = 255;
+const int GREEN_VAL = 255;
+const int BLUE_VAL = 255;
+const int MID_VAL = 128;
+
 // starting w struct, will change to class
 class Color {
     private:
     int r, g, b;
     
     public:
-    Color() { r = 0; g = 0; b = 0; } 
+    Color() {
+        r = ZERO_VAL;
+        g = ZERO_VAL;
+        b = ZERO_VAL;
+    } 
 
-    Color(int r, int g, int b){
-        r = r; // using same parameter names here
-        g = g;
-        b = b;
+    Color(int rr, int gg, int b){
+        r = rr;
+        g = gg;
+        b = bb;
     }
+    
     void setRed(int value) { r = value }
     void setGreen(int value) { g = value; }
     void setBlue(int value) { b = value; }
@@ -22,20 +33,17 @@ class Color {
     int getGreen() { return g; }
     int getBlue() { return b; }
      
-    void print() {
-        cout << "Color(" << r << ", " << g << ", " << b << ")\n";
+   // print color values
+   void print() {
+        cout << "Red: " << r
+             << "  Green: " << g
+             << "  Blue: " << b << endl;
     }
 };
 
 int main() {
-    Color c1{255, 0, 0}; // testing colors
-    Color c2;
-    c2.setRed(0);
-    c2.setGreen(255);
-    c2.setBlue(0);
-
+   Color c1(MID_VAL, MID_VAL, MID_VAL);
     c1.print();
-    c2.print();
     return 0;
 }
 
