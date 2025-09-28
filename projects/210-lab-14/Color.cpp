2b1b3a5240016dfab2ca53a05d7ffd8b563f43b2
1080heidi <heidideleon415@gmail.com>
2025-09-27 17:39:03 -0700

    Seperating files

---
+++ projects/210-lab-14/Color.cpp
@@ -0,0 +1,32 @@
+#include "Color.h"
+#include <iostream>
+using namespace std;
+
+// default constructor
+Color() {
+    r = ZERO_VAL;
+    g = ZERO_VAL;
+    b = ZERO_VAL;
+}
+ 
+// constructor with parameters
+Color::Color(int rr, int gg, int bb){
+    r = rr;
+    g = gg;
+    b = bb;
+    }
+
+void Color::setRed(int value) { r = value }
+void Color::setGreen(int value) { g = value; }
+void Color::setBlue(int value) { b = value; }
+    
+int Color::getRed() { return r;}
+int Color::getGreen() { return g; }
+int Color::getBlue() { return b; }
+     
+   // print color values
+void Color::print() {
+     cout << "Red: " << r
+          << "  Green: " << g
+          << "  Blue: " << b << endl;
+    }
--- projects/210-lab-14/main.cpp
+++ projects/210-lab-14/main.cpp
@@ -7,43 +7,19 @@ const int GREEN_VAL = 255;
 const int BLUE_VAL = 255;
 const int MID_VAL = 128;
 
-// starting w struct, will change to class
-class Color {
-    private:
-    int r, g, b;
-    
-    public:
-    Color() {
-        r = ZERO_VAL;
-        g = ZERO_VAL;
-        b = ZERO_VAL;
-    } 
-
-    Color(int rr, int gg, int b){
-        r = rr;
-        g = gg;
-        b = bb;
-    }
-    
-    void setRed(int value) { r = value }
-    void setGreen(int value) { g = value; }
-    void setBlue(int value) { b = value; }
-    
-    int getRed() { return r;}
-    int getGreen() { return g; }
-    int getBlue() { return b; }
-     
-   // print color values
-   void print() {
-        cout << "Red: " << r
-             << "  Green: " << g
-             << "  Blue: " << b << endl;
-    }
 };
 
 int main() {
-   Color c1(MID_VAL, MID_VAL, MID_VAL);
-    c1.print();
+   Color black(ZERO_VAL, ZERO_VAL, ZERO_VAL);
+   Color red(RED_VAL, ZERO_VAL, ZERO_VAL);
+   Color green(ZERO_VAL, GREEN_VAL, ZERO_VAL);
+   Color blue(ZERO_VAL, ZERO_VAL, BLUE_VAL);
+
+    black.print();
+    red.print();
+    green.print();
+    blue.print();
+
     return 0;
 }
 
